777d16669c89b4e595a3d6d6356ea979df9a37b7
Samuel Escapa <samuel.escapa@gmail.com>
2020-02-13 08:00:44 -0800

    Add routes

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Search() {
+  return (
+    <div>
+      Search:
+      <br></br>
+      <Link to="/product/1">Product 1 Details</Link>
+    </div>
+  );
+}
+
+export default Search;
