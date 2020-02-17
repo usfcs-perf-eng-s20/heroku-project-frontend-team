8b97a8c4b4d5019ab7c203ba3fea462a93abb0cd
pi31415926 <st271828@sina.com>
2020-02-16 21:35:44 -0800

    added tabs and separated routes to routes.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+import Routes from './routes';
 
-import HelloWorld from "./components/HelloWorld";
-import Home from "./components/Home";
-import Product from "./components/Product";
-import Profile from "./components/Profile";
-import Search from "./components/Search";
-import MyTop from "./components/MyTop";
+import NavBar from './NavBar';
 
 function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-          <li>
-            <Link to="/top">My top</Link>
-          </li>
-          <li>
-            <Link to="/me">Profile</Link>
-          </li>
-        </ul>
-      </div>
-      <Switch>
-        <Route path="/helloworld">
-          <HelloWorld />
-        </Route>
-        <Route path="/product/:id">
-          <Product />
-        </Route>
-        <Route path="/me">
-          <Profile />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/top">
-          <MyTop />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <NavBar />
+      <Routes />
     </Router>
   );
 }
