5f7ca7dab45eeca77ec78a20d8c87ea180887574
Samuel Escapa <samuel.escapa@gmail.com>
2020-02-25 08:38:00 -0800

    Add MyTop page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router} from "react-router-dom";
-import Routes from './routes';
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./components/routes";
 
-import NavBar from './NavBar';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
