6d8d33b3c3c2c36d1418eef2b2ad3822437e2d2b
pi31415926 <st271828@sina.com>
2020-03-02 00:14:47 -0800

    implemented home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Coverflow from '@stuuno/node-flow';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Coverflow
+      displayQuantityOfSide={2}
+      navigation
+      infiniteScroll
+      enableHeading
+      media={{
+        '@media (max-width: 600px)': {
+          height: '600px'
+        },
+        '@media (min-width: 100px)': {
+          height: '600px'
+        }
+      }}
+      >
+      <img src='https://www.joblo.com/assets/images/joblo/posters/2019/01/IO-poster-1.jpg'/>
+      <img src='https://www.mauvais-genres.com/26247/avengers-endgame-original-movie-poster-15x21-in-2019-anthony-russo-robert-downey-jr.jpg'/>
+      <img src='https://images.moviepostershop.com/replicas-movie-poster-1000778791.jpg'/>
+      <img src='https://www.arthipo.com/image/cache/catalog/genel-tasarim/all-posters/sinema-cinema-film-postersleri/yabanci-filmler/1/pfilm1505-avatar-2_c02bbf7f-poster-movie-film-1000x1000.jpg'/>
+      </Coverflow>
+    </div>
+  );
 }
 
 export default Home;
