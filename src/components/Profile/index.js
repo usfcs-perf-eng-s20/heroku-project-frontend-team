8181067f94f75b7cbd98f321273e8b05d22582b1
pi31415926 <st271828@sina.com>
2020-03-02 16:09:19 -0800

    try to implement profile but met CORS issue

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from "react";
+import Button from '@material-ui/core/Button';
+import ProfileAPICalls from './profileAPICalls';
 
 function Profile(props) {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [profileName, setProfileName] = useState("Samuel");
+  useState(()=>{
+    try {
+      setInterval(() => {
+        ProfileAPICalls.getMyProfile(0).then(
+            (v) => {
+              console.log( v );
+            },
+            (error) => {
+              console.log( error );
+            }
+        );
+      }, 3000);
+    } catch(e) {
+      console.log(e);
+    }
+    
+  })
+  const [profileName, setProfileName] = useState("Perf");
+  const [age, setAge] = useState(50);
+  const [email, setEmail] = useState("perfeng.loginteam@gmail.com");
+  const [city, setCity] = useState("San Francisco");
+
+  const [newProfileName, setNewProfileName] = useState(undefined);
+  const [newAge, setNewAge] = useState(undefined);
+  const [newEmail, setNewEmail] = useState(undefined);
+  const [newCity, setNewCity] = useState(undefined);
 
   useEffect(() => {
     console.log(profileName);
@@ -10,15 +36,29 @@ function Profile(props) {
 
   return (
     <div>
-      Profile
+      Hello, {profileName}
       <br />
-      {searchQuery}
+      Age: {age} 
       <br />
-      {profileName}
+      Email: {email} 
       <br />
-      <input onChange={e => setSearchQuery(e.target.value)}></input>
+      City: {city} 
       <br />
-      <input onChange={e => setProfileName(e.target.value)}></input>
+      <br />
+      Change your name:
+      <input onChange={e => setNewProfileName(e.target.value)}></input>
+      <br />
+      Change your age:
+      <input onChange={e => setNewAge(e.target.value)}></input>
+      <br />
+      Change your email:
+      <input onChange={e => setNewEmail(e.target.value)}></input>
+      <br />
+      Change your city:
+      <input onChange={e => setNewCity(e.target.value)}></input>
+      <br />
+      <Button style={{'color':'green'}} onClick={()=>{}}>Submit</Button>
+      <Button style={{'color':'red'}} onClick={()=>{setNewProfileName(undefined);setNewAge(undefined);setNewEmail(undefined);setNewCity(undefined)}}>Cancel</Button>
     </div>
   );
 }
