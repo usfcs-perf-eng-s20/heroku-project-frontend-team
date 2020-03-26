bda1c054f86b4cafb5da6d4535a1b7e45479cdef
pi31415926 <st271828@sina.com>
2020-03-26 10:24:00 -0700

    implemented TopMovies

--- src/components/MyTop/components/TopMovies.js
+++ src/components/MyTop/components/TopMovies.js
@@ -1,44 +1,52 @@
 import React, { useState } from "react";
-
+import MyTopApicalls from "../MyTopApicalls";
 import "./TopMovies.css";
 
-const data = {
-  body: [
-    {
-      title: "Gone With The Wind",
-      favs: 13,
-      rating: 4.5,
-      checkouts: 112
-    }
-  ],
-  ids: ["Gone With The Wind"]
-};
+
 
 function TopMovies() {
-  const [view, setView] = useState(0);
 
+  const [view, setView] = useState(0);
+  const [data, setData] = useState([]);
+
+  
   const selectCorrectView = (favs, rating, checkouts) => {
     if (view === 0) return favs;
     if (view === 1) return rating;
     if (view === 2) return checkouts;
   };
 
+  const refreshHandler = () =>{
+    MyTopApicalls.getMyTops().then(
+      (v) => {
+          console.log( "@getMyTops Succeeded@", v );
+          setData(v);
+          console.log( "@data@", data);
+      },
+      (error) => {
+          console.log( error );
+      }
+    );
+  }
+
   return (
     <div>
+      {/* {refreshHandler()} */}
       <h1>Top Movies</h1>
-      <button onClick={() => setView(0)}>Favs</button>
-      <button onClick={() => setView(1)}>Rating</button>
-      <button onClick={() => setView(2)}>Checkouts</button>
+      <button onClick={refreshHandler}>Refresh</button>
+      <button onClick={() => setView(0)}>sumOfRatings</button>
+      <button onClick={() => setView(1)}>totalCountOfRatings</button>
+      <button onClick={() => setView(2)}>numberOfFavorites</button>
       <table>
         <tbody>
           <tr>
             <td>Movie</td>
-            <td>{view === 0 ? "Favs" : view === 1 ? "Rating" : "Checkouts"}</td>
+            <td>{view === 0 ? "sumOfRatings" : view === 1 ? "totalCountOfRatings" : "numberOfFavorites"}</td>
           </tr>
-          {data.body.map(({ title, favs, rating, checkouts }) => (
-            <tr key={title}>
-              <td>{title}</td>
-              <td>{selectCorrectView(favs, rating, checkouts)}</td>
+          {data.map(({ id, sumOfRatings, totalCountOfRatings, numberOfFavorites }) => (
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{selectCorrectView(sumOfRatings, totalCountOfRatings, numberOfFavorites)}</td>
             </tr>
           ))}
         </tbody>
